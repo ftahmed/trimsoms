18f544fb80780a81173d1f228b4d47ff6cea57fd
ftahmed <ftahmed@gmail.com>
2023-02-07 01:45:49 +0600

    Form based login

---
+++ src/main/java/me/ftahmed/bootify/config/SecSecurityConfig.java
@@ -0,0 +1,70 @@
+package me.ftahmed.bootify.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig {
+
+    @Bean 
+    public PasswordEncoder passwordEncoder() { 
+        return new BCryptPasswordEncoder(); 
+    }
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+            .password(passwordEncoder().encode("user1Pass"))
+            .roles("USER")
+            .build();
+        UserDetails user2 = User.withUsername("user2")
+            .password(passwordEncoder().encode("user2Pass"))
+            .roles("USER")
+            .build();
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder().encode("adminPass"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests()
+                .requestMatchers("/admin/**")
+                    .hasRole("ADMIN")
+                .requestMatchers("/anonymous*")
+                .   anonymous()
+                .requestMatchers("/login*", "/js/**", "/css/**", "/images/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/", true)
+                    .failureUrl("/login?error=true")
+                    // .failureHandler(authenticationFailureHandler())
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .deleteCookies("JSESSIONID")
+                    // .logoutSuccessHandler(logoutSuccessHandler())
+            ;
+        // .and()
+        // .exceptionHandling().accessDeniedPage("/accessDenied");
+        // .exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+        return http.build();
+    }
+}
